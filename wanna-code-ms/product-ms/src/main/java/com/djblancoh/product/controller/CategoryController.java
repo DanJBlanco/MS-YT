56257c8a015be7b5cb148b560e89edef9c820972
Daniel <djblancoh@gmail.com>
2023-06-14 10:42:59 +0200

    add config value to get value in execution

---
+++ wanna-code-ms/product-ms/src/main/java/com/djblancoh/product/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.djblancoh.product.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/categories")
+@Getter
+@Setter
+public class CategoryController {
+
+    @Value("${app.test-prop}")
+    private String testProp;
+
+    public String getTestProp(){
+        return this.testProp;
+    }
+
+
+}
