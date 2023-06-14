9640fbd4ccb4e4783413ea9b8b7e5fd87023ddb1
Daniel <djblancoh@gmail.com>
2023-06-14 10:52:57 +0200

    add tag @RefreshScope

--- wanna-code-ms/product-ms/src/main/java/com/djblancoh/product/controller/CategoryController.java
+++ wanna-code-ms/product-ms/src/main/java/com/djblancoh/product/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.djblancoh.product.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/categories")
 @Getter
 @Setter
+@RefreshScope
 public class CategoryController {
 
     @Value("${app.test-prop}")
