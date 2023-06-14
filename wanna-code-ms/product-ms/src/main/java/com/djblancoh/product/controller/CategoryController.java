ffad1c3871964bbc618ad04d495f60a1fc6bf33c
Daniel <djblancoh@gmail.com>
2023-06-14 10:50:15 +0200

    Fix config controller

--- wanna-code-ms/product-ms/src/main/java/com/djblancoh/product/controller/CategoryController.java
+++ wanna-code-ms/product-ms/src/main/java/com/djblancoh/product/controller/CategoryController.java
@@ -3,11 +3,12 @@ package com.djblancoh.product.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/categories")
+@RequestMapping("/api/categories")
 @Getter
 @Setter
 public class CategoryController {
@@ -15,6 +16,7 @@ public class CategoryController {
     @Value("${app.test-prop}")
     private String testProp;
 
+    @GetMapping("/test-prop")
     public String getTestProp(){
         return this.testProp;
     }
