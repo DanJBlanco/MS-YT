a10554f41340feb0028fdba2a3e2f8465717db81
Daniel <djblancoh@gmail.com>
2023-06-15 13:27:17 +0200

    update to disable oauthw

---
+++ wanna-code-ms/api-gateway/src/main/java/com/djblancoh/apigateway/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.djblancoh.apigateway;
+
+//@Configuration
+//@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+//        httpSecurity.authorizeExchange( exchange -> exchange.anyExchange().authenticated())
+//                .oauth2Login(Customizer.withDefaults());
+//
+//        httpSecurity.csrf().disable();
+//
+//        return httpSecurity.build();
+//    }
+}
---
+++ wanna-code-ms/product-ms/src/main/java/com/djblancoh/product/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.djblancoh.product;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+public class SecurityConfiguration {
+
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//
+//        httpSecurity.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+//        return httpSecurity.build();
+//
+//    }
+
+
+}
